1e61237efe225d50b35490c717d892ef706e0de2
NoScxped <acolesios@icloud.com>
2022-12-04 21:41:11 -0500

    bug fix

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 exports.susify = function (string){
 
     const regex = /among us|amongus|amogus|sus|sussy/g;
-    return string.replaceAll(regex, 'ඞඞඞ')
+    return string.toLowerCase().replaceAll(regex, 'ඞඞඞ')
 
 }
